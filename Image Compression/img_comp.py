7fe869f06d096893976eeca22fa2350efd75ee72
Aslı Akın <slkn521@gmail.com>
2026-03-07 17:49:43 +0300

    Image compression added

---
+++ Image Compression/img_comp.py
@@ -0,0 +1,39 @@
+from PIL import Image
+import os
+
+# Upload Image
+img_org = Image.open(r"C:\Users\slkn5\Desktop\Deep Learning\Image Compression\bird-8788491_1280.jpg")
+
+# Load image as array
+img_org_arr = img_org.load()
+
+# Crop Image
+# Define coordinates
+x1 = 680 # Beginning of X
+y1 = 160 # Beginning of Y
+x2 = 1180 # End of X
+y2 = 720 # End of Y
+
+# Define a new image
+cropped_width = x2 - x1
+cropped_height = y2 - y1
+cropped_img = Image.new("RGB", (cropped_width, cropped_height))
+cropped_img_arr = cropped_img.load()
+
+# Crop the image
+for i in range(x1,x2):
+    for j in range(y1,y2):
+        cropped_img_arr[i - x1, j - y1] = img_org_arr[i, j]
+
+# Display cropped image
+cropped_img.show()
+
+# Compress cropped image
+cropped_img.save("compressed_cropped_img.jpg", quality = 30, optimize = True)
+
+# Show file size of original image and compressed image
+org_size = os.path.getsize("Image Compression/bird-8788491_1280.jpg")
+compressed_size = os.path.getsize("compressed_cropped_img.jpg")
+
+print(f"Original file size: {org_size/1024:.2f} KB")
+print(f"Compressed file size: {compressed_size/1024:.2f} KB") # File size significantly decreased due to compression process as expected. 
